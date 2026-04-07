2b693522681ef51390bee42ead0f8aa21389b6e8
Devpt1904 <devp04412@gmail.com>
2026-04-07 15:12:27 +0530

    resume

--- isha/app/layout.tsx
+++ isha/app/layout.tsx
@@ -17,7 +17,12 @@ const inter = Inter({
 });
 
 export const metadata: Metadata = {
-  metadataBase: new URL("http://localhost:3000"),
+  metadataBase: new URL(
+    process.env.NEXT_PUBLIC_SITE_URL ??
+      (process.env.VERCEL_URL
+        ? `https://${process.env.VERCEL_URL}`
+        : "http://localhost:3000"),
+  ),
   title: {
     default: DATA.home.hero.name,
     template: `%s | ${DATA.home.hero.name}`,
